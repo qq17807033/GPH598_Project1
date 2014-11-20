c08625cecf0aa4911d777a999a1cfac0f6a6cd7e
qq17807033 <17807033@qq.com>
2014-11-20 13:12:24 -0700

    Update FileIO.py

--- FileIO.py
+++ FileIO.py
@@ -9,3 +9,17 @@ import pysal
 def openFile(connectionString, mode='r'):
     shp = pysal.pysal.core.FileIO.FileIO.open(connectionString, mode='r')  # @UndefinedVariable
     shp.seek(0)
+    
+@author: Kailai 
+
+import pysal
+shp = pysal.open('E:/PUP598 geocomputation/Cities.shp')
+print len(shp)
+
+import pysal
+dbf = pysal.open('E:/PUP598 geocomputation/Cities.dbf')
+print len(dbf)
+
+import pysal
+csv = pysal.open('E:/PUP598 geocomputation/Cities.csv')
+print len (csv)
